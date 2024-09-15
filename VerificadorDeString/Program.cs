5e6017be857bd959015833b1ba530fb03fd602bc
Kayky1812 <Kaykysantos181203@gmail.com>
2024-09-15 19:46:39 -0300

    Meu primeiro commit

---
+++ VerificadorDeString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Digite uma string:");
+        string input = Console.ReadLine();
+
+        
+        int count = ContarOcorrenciasDeA(input);
+
+        if (count > 0)
+        {
+            Console.WriteLine($"A letra 'a' aparece {count} vezes na string.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' não aparece na string.");
+        }
+    }
+
+   
+    static int ContarOcorrenciasDeA(string str)
+    {
+        int contador = 0;
+
+        foreach (char c in str)
+        {
+            if (c == 'a' || c == 'A')
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+}
